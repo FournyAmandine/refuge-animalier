31ebd0f534d918c8811e01a62e5d4802efc16e1f
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-19 14:27:54 +0100

    Added app.blade.php with the footer and the header

---
+++ resources/views/components/structure/header.blade.php
@@ -0,0 +1,13 @@
+<header class="m-[0.9375rem] pb-[0.625rem] border-b-2 border-orange-700">
+    <a class="go_back" href="javascript:history.go(-1)" title="Retourner sur la page précédente"><span>Retour</span></a>
+    <div class="flex items-end justify-between">
+
+        <x-navigation.burger_menu>
+
+        </x-navigation.burger_menu>
+
+        <a href="{!! route('public.homepage') !!}" title="Revenir à la page d'accueil"><img width="174" height="59" src="assets/img/logo-tel.svg" alt="Logo Les Pattes Heureuses avec une patte de chat dans une maison"></a>
+
+        <a class="header_animaux" href="{!! route('public.homepage') !!}" title="Voir les animaux"><span>Voir les animaux</span></a>
+    </div>
+</header>
