c4662799ea0772b82c3c99e70d94531c2d5b7c71
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-19 14:28:51 +0100

    Added the burger menu

---
+++ resources/views/components/navigation/burger_menu.blade.php
@@ -0,0 +1,16 @@
+<input class="nav__check" type="checkbox" id="menu-toggle" />
+<label class="nav__toggle" aria-label="Ouvrir le menu principal" for="menu-toggle">
+    <svg class="nav__svg" width="30" height="30" viewBox="0 0 30 30" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+        <path class="nav__path" d="M6.25 9.30469H23.75" stroke-width="2"></path>
+        <path class="nav__path" d="M6.25 15.554H23.75" stroke-width="2"></path>
+        <path class="nav__path" d="M6.25 21.8047H23.75" stroke-width="2"></path>
+    </svg>
+</label>
+<ul class="nav__container">
+    <li><a href="#">Nos compagnons</a></li>
+    <li><a href="#"></a>Le refuge</li>
+    <li><a href="#"></a>Devenir bénévole</li>
+    <li><a href="#">Nous contacter</a></li>
+    <li><a href="#">Adopter</a></li>
+</ul>
