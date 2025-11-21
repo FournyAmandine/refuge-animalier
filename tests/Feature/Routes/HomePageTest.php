ddf8c76e95fe9790f9f700b429c7c7188094f1b7
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-21 13:56:38 +0100

    Change the name of the test

--- tests/Feature/Routes/HomePageTest.php
+++ tests/Feature/Routes/HomePageTest.php
@@ -1,10 +1,9 @@
 <?php
 
-it('verifies if the public.homepage is dislays correctly the homepage', function (){
+it('verifies if the public.homepage dislays correctly the homepage', function (){
 
     $response = \Pest\Laravel\get(route('public.homepage'));
 
     $response->assertStatus(200);
-    //$response->assertSee("<h2>Offrez-leur une seconde chance, ils vous rendront un amour infini !</h2>");
 
 });
