8a5867c04b030acacadd2d5ca5e004620e369548
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-21 13:56:02 +0100

    Update app title

--- resources/views/components/public/app.blade.php
+++ resources/views/components/public/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="author" content="Amandine Fourny">
     <meta name="description" content="Site pour un refuge animalier">
     <meta name="keywords" content="refuge, animal, adoption, charly">
-    <title>{!! $title_page !!}</title>
+    <title>{!! $title_page !!} - Les Pattes Heureuses</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-orange-50">
