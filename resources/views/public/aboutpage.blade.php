f8282e37bd290f65b37d73a30e07548d58eb2d7a
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-21 16:56:02 +0100

    Create my aboutpage.blade.php

--- resources/views/public/aboutpage.blade.php
+++ resources/views/public/aboutpage.blade.php
@@ -1,39 +1,91 @@
+@php
+    $history=[
+        'Sensibiliser le public à la cause animale,', 'Encourager l’adoption responsable,', 'Promouvoir la stérilisation et la vaccination,', 'Offrir un accompagnement bienveillant aux familles adoptantes.'
+    ];
+
+    $actions = [
+        'Accueillons les animaux abandonnés ou signalés,', 'Leur apportons soins, nourriture et attention,', 'Gérons les demandes d’adoption,', 'Organisons des rencontres entre adoptants et animaux,', 'Maintenons un contact régulier avec les nouvelles familles pour assurer le suivi.'
+    ]
+@endphp
+
+
 <x-public.app>
     <x-slot:title_page>
         Notre refuge
     </x-slot:title_page>
     <main>
-        <x-public.sections.text-media
-            section_title="Notre refuge"
-            content="Le refuge Les Pattes Heureuses œuvre chaque jour pour le bien-être animal. Grâce à une équipe de bénévoles dévoués, nous assurons l’accueil, les soins, la réhabilitation et l’adoption responsable de nombreux animaux. Depuis sa création, le refuge Les Pattes Heureuses s’engage à offrir protection, soins et amour aux animaux abandonnés. Ici, chaque patte compte, chaque histoire mérite une fin heureuse."
-            flex="flex-row-reverse"
-            px="px-8"
+        <span class="ariane pl-5 text-sm text-orange-600 font-semibold italic pb-5">Refuge</span>
+
+        <h2 class="pl-5 [font-size:var(--text-3xl)] text-orange-600 underline decoration-orange-400 decoration-3 py-5">Découvrez notre refuge</h2>
+
+        <x-public.sections.text-media-list
+            :has_main_title="true"
+            section_title="Notre histoire"
+            content="Notre objectif est simple : protéger, soigner et replacer chaque animal dans un foyer adapté et aimant. Mais au-delà de l’adoption, nous croyons en une mission plus large&nbsp;:"
+            class_button="px-3 absolute left-0 z-2"
             href="#"
-            title="Allez vers la page de notre refuge"
-            label="En apprendre plus"
-            :src="asset('assets/img/chien_1.png')"
-            alt="illustration d'un chien corgi assis brun et blanc"
-            width_img="137"
-            heigth_img="207"
-            position_x="left-7"
-            position_top="top-[-3rem]"
+            title="Allez vers la page d'adoption'"
+            label="Devenir un foyer accueillant"
+            :button="true"
+            :src="asset('assets/img/chat_1.png')"
+            alt="illustration d'un chat chat blanc, gris, brun assis"
+            width_img="170"
+            heigth_img="223"
+            :src_patte="asset('assets/img/patte-right_medium.svg')"
+            :items="$history"
+            class_img="absolute right-0 top-[-3rem]"
+        />
+
+        <x-public.sections.text-media
+            :has_main_title="true"
+            section_title="Notre équipe"
+            content="Le refuge vit grâce à une petite équipe passionnée&nbsp;:&nbsp;des bénévoles dévoués, des familles d’accueil, et bien sûr Élise, la fondatrice, qui supervise les soins, les adoptions et la gestion quotidienne.Chaque membre apporte son énergie, son amour et ses compétences pour garantir le bien-être de nos pensionnaires."
+            class_button="px-5"
+            class="flex-row-reverse"
+            :button="true"
+            href="#"
+            title="Allez vers la page bénévole"
+            label="Devenir bénévole"
+            :src="asset('assets/img/chat_2.png')"
+            alt="illustration d'un chat gris, roux et brun assis et nous regarde"
+            width_img="160"
+            heigth_img="185"
+            class_img="absolute left-2 top-[-4rem]"
             :src_patte="asset('assets/img/patte-left_medium.svg')"
         />
-        <x-public.sections.text-media
-            section_title="Être bénévole"
-            content="Être bénévole au refuge Les Pattes Heureuses, c’est participer à une mission pleine de sens : offrir des soins, des promenades, des jeux et surtout de l’affection à nos pensionnaires. Que vous ayez une heure ou une journée, votre présence compte. Ensemble, faisons rimer entraide et bonheur animal."
-            flex="flex-row"
-            px="px-3"
-            href="#"
-            title="Allez vers la page de bénévolat"
-            label="Devenir bénévole"
-            :src="asset('assets/img/chien_2.png')"
-            alt="illustration d'un chien labrador assis brun, noir et blanc"
-            width_img="180"
-            heigth_img="180"
-            position_x="right-0"
-            position_top="top-[-5rem]"
+
+        <x-public.sections.text-media-list
+            :has_main_title="true"
+            section_title="Nos actions au quotidien"
+            content="Chaque jour, nous&nbsp;:"
+            class_button="px-3 absolute left-0"
+            :button="false"
+            :src="asset('assets/img/chat_3.png')"
+            alt="illustration d'un chat gris"
+            width_img="170"
+            heigth_img="170"
             :src_patte="asset('assets/img/patte-right_medium.svg')"
+            :items="$actions"
+            class_img="absolute right-0 top-[-4rem]"
         />
+
+        <x-public.sections.text-media
+            :has_main_title="true"
+            section_title="Notre philosophie"
+            content="Nous croyons que chaque animal mérite une vie douce et respectueuse. Chaque adoption est une rencontre unique, une histoire qui commence. Aux Pattes Heureuses, nous prenons le temps de connaître nos compagnons pour leur offrir la famille qui leur correspond vraiment."
+            class_button="px-4.5"
+            class="flex-row-reverse"
+            :button="true"
+            href="#"
+            title="Allez vers la page animaux"
+            label="Voir nos compagnons"
+            :src="asset('assets/img/chat_4.png')"
+            alt="illustration d'un chat gris, roux et brun assis et nous regarde"
+            width_img="140"
+            heigth_img="190"
+            class_img="absolute left-2 top-[-4rem]"
+            :src_patte="asset('assets/img/patte-left_medium.svg')"
+        />
+
     </main>
 </x-public.app>
