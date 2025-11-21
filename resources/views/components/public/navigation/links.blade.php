4633af9ee8919c921c7b590279da99abef586cab
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-21 13:58:43 +0100

    Push a new route in links table

--- resources/views/components/public/navigation/links.blade.php
+++ resources/views/components/public/navigation/links.blade.php
@@ -2,7 +2,7 @@
 @php
     $links = [
         ['li_class' => $li_class, 'href'=>'#', 'label'=>'Nos compagnons', 'title'=>'Aller vers la page des compagnons'],
-        ['li_class' => $li_class, 'href'=>'#', 'label'=>'Le refuge', 'title'=>'Aller vers la page du refuge'],
+        ['li_class' => $li_class, 'href'=>route('public.aboutpage'), 'label'=>'Le refuge', 'title'=>'Aller vers la page du refuge'],
         ['li_class' => $li_class, 'href'=>'#', 'label'=>'Devenir bénévole', 'title'=>'Aller vers la page de bénévole'],
         ['li_class' => $li_class, 'href'=>'#', 'label'=>'Nous contacter', 'title'=>'Aller vers la page de contact'],
         ['li_class' => $li_class, 'href'=>'#', 'label'=>'Adopter', 'title'=>'Aller vers la page d’adoption'],
@@ -10,7 +10,7 @@
 @endphp
 
 
-<ul class="{!! $nav_class !!}">
+<ul class="{!! $nav_class !!} z-2">
     @foreach($links as $link)
         <x-public.navigation.link li_class="{!! $li_class !!}" :href="$link['href']" :label="$link['label']" :title="$link['title']"/>
     @endforeach
