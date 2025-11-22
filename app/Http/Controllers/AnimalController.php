3b791f511b06670208f7fe7f12d1db72f8c82f10
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-22 12:11:44 +0100

    Add function index in controller

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Enum\AnimalStatus;
+use App\Models\Animal;
+
 class AnimalController extends Controller
 {
     public function index()
     {
-
+        $animals = Animal::where('state', [AnimalStatus::Available])->paginate(8);
+        return view('public.animals.index', compact('animals'));
     }
 }
