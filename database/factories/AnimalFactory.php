bee45c5d38257bbee8990d24cb6ae42a92c09e69
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-22 12:10:28 +0100

    Add state in my factory

--- database/factories/AnimalFactory.php
+++ database/factories/AnimalFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enum\AnimalStatus;
 use App\Models\Animal;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
@@ -16,6 +17,8 @@ class AnimalFactory extends Factory
         $name = ['Charly', 'Frimousse', 'Happy', 'Panpan', 'Moka', 'Simba', 'Bouboule', 'Bunny', 'Folette'];
         $sexe = ['Mâle', 'Femelle'];
         $race = ['Chat', 'Lapin', 'Border Collie', 'American Staff', 'Caniche', 'Golden Retriever'];
+        $state = [AnimalStatus::Adopted, AnimalStatus::Available, AnimalStatus::Care, AnimalStatus::Draft, AnimalStatus::Pending];
+        $img_path = ['assets/img/bunny.png', 'assets/img/bouboule.png', 'assets/img/charly.png', 'assets/img/folette.png', 'assets/img/happy.png', 'assets/img/simba.png', 'assets/img/moka.png', 'assets/img/panpan.png'];
 
         return [
             'name' => $this->faker->randomElement($name),
@@ -23,6 +26,8 @@ class AnimalFactory extends Factory
             'birth_date' => $this->faker->date(),
             'age' => $this->faker->numberBetween(1, 8),
             'race' => $this->faker->randomElement($race),
+            'state' => $this->faker->randomElement($state),
+            'img_path' => $this->faker->randomElement($img_path),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
