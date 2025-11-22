bcb8ea15fd0846512b3f6d0a2c6792ca0bcebb79
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-22 14:43:08 +0100

    Did the index page with the animals in database

--- resources/views/public/animals/index.blade.php
+++ resources/views/public/animals/index.blade.php
@@ -2,4 +2,18 @@
     <x-slot:title_page>
         Nos compagnons
     </x-slot:title_page>
+    <main>
+        <span class="ariane pl-5 text-sm text-orange-600 font-semibold italic pb-5">Compagnons</span>
+        <h2 class="p-5 [font-size:var(--text-3xl)] text-orange-600 underline decoration-orange-400 decoration-3 text-center">Découvrez nos petits compagnons</h2>
+       <x-public.sections.search/>
+        <section class="pt-10 flex flex-col gap-10 items-center">
+            @foreach($animals as $animal)
+                <x-public.cards.card src="{!! $animal->img_path !!}"
+                                     alt="Photo de {!! $animal->name !!}"
+                                     title="{!! $animal->name !!}"
+                                     href="#"
+                                     name="{!! $animal->name !!}" :dd="[$animal->sexe,$animal->age . 'ans',$animal->race]"/>
+            @endforeach
+        </section>
+    </main>
 </x-public.app>
