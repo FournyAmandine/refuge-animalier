6072de8f86f0836c4714e3e424e076e69792a3b7
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-23 17:13:01 +0100

    Make the view

---
+++ resources/views/public/adoptionpage.blade.php
@@ -0,0 +1,22 @@
+<x-public.app>
+    <x-slot:title_page>
+        Devenir bénévole
+    </x-slot:title_page>
+    <main class="contact_form pb-11">
+        <x-public.sections.intro title="Devenez bénévole&nbsp;!" ariane="Refuge/Bénévole"/>
+        <x-public.sections.form>
+            <x-public.form.fields.input field_name="last_name" required="required" placeholder="Doe" label="Entrez votre nom"/>
+            <x-public.form.fields.input field_name="first_name" required="required" placeholder="John" label="Entrez votre prénom"/>
+            <x-public.form.fields.input type="email" field_name="email" required="required" placeholder="John@doe.be" label="Entrez votre email"/>
+            <x-public.form.fields.textarea field_name="message" label="Pourquoi devenir bénévole?" placeholder="J'aimerai devenir bénévole pour..."/>
+            <x-slot:text>
+                Rejoindre Les Pattes Heureuses, c’est bien plus que donner un peu de son temps : c’est offrir de la tendresse, du réconfort et une seconde chance à des animaux qui n’attendent qu’un regard bienveillant.  Devenez bénévole et partagez cette belle aventure humaine et animale à nos côtés !            </x-slot:text>
+            <x-slot:button_label>
+                Devenir bénévole
+            </x-slot:button_label>
+        </x-public.sections.form>
+        <img class="absolute bottom-[-6rem] px-5" src="{!! asset('assets/img/lapin_benevole.png') !!}" alt="Illustration d'un golden retriever" width="180" height="206">
+    </main>
+</x-public.app>
+
+
