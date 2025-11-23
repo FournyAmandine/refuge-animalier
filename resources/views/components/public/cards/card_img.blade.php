0f1784bec8bb31823d5f5148a9bf093375205612
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-23 10:28:20 +0100

    Small changes

--- resources/views/components/public/cards/card_img.blade.php
+++ resources/views/components/public/cards/card_img.blade.php
@@ -1,9 +1,9 @@
 @props(['src', 'alt', 'name'])
 
 <figure>
-    <img class="rounded-t-xl" src="{!! $src !!}" alt="{!! $alt !!}" width="306" height="192">
+    <img class="rounded-t-lg" src="{!! $src !!}" alt="{!! $alt !!}" width="306" height="192">
 </figure>
-<div class="absolute inset-0 bg-gradient-to-tr from-black/80 to-transparent rounded-t-xl"></div>
+<div class="absolute inset-0 bg-gradient-to-tr from-black/80 to-transparent rounded-t-lg"></div>
 <div class="absolute left-4 top-36 flex gap-4">
     <h3 class=" text-orange-50 font-bold text-2xl underline decoration-orange-400 decoration-2">
         {!! $name !!}
