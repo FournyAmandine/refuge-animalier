69430a35e9deb9e4fb56fdf71758d5b06a98f36c
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-23 14:18:23 +0100

    Update component for the intro

--- resources/views/components/public/sections/intro.blade.php
+++ resources/views/components/public/sections/intro.blade.php
@@ -1,8 +1,8 @@
-@props(['title'])
+@props(['title, ariane'])
 
 
 <div>
-    <span class="ariane pl-5 text-sm text-orange-600 font-semibold italic pb-5">Contact</span>
+    <span class="ariane pl-5 text-sm text-orange-600 font-semibold italic pb-5">{!! $ariane !!}</span>
     <h2 class="p-5 [font-size:var(--text-3xl)] text-orange-600 underline decoration-orange-400 decoration-3 flex justify-center">
         {!! $title !!}
     </h2>
