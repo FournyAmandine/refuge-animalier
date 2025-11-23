56d779a4098b06c04192e1fc24113c500eab3233
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-23 13:59:15 +0100

    Make the view contactpage.blade.php

---
+++ resources/views/public/contactpage.blade.php
@@ -0,0 +1,10 @@
+<x-public.app>
+    <x-slot:title_page>
+        Contact
+    </x-slot:title_page>
+    <main>
+        <form action="" method="get">
+
+        </form>
+    </main>
+</x-public.app>
