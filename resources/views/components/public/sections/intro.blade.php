73f40f47d7f5ad49d3c088f79006277b909cba40
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-23 14:17:31 +0100

    Make a new component for the intro

---
+++ resources/views/components/public/sections/intro.blade.php
@@ -0,0 +1,9 @@
+@props(['title'])
+
+
+<div>
+    <span class="ariane pl-5 text-sm text-orange-600 font-semibold italic pb-5">Contact</span>
+    <h2 class="p-5 [font-size:var(--text-3xl)] text-orange-600 underline decoration-orange-400 decoration-3 flex justify-center">
+        {!! $title !!}
+    </h2>
+</div>
