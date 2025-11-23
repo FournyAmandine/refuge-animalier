a1bd9a81a8fcd9f330dbaf12b115b3d50373c282
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-23 16:06:42 +0100

    Add RefreshData

--- tests/Feature/Routes/AnimalIndexTest.php
+++ tests/Feature/Routes/AnimalIndexTest.php
@@ -1,5 +1,10 @@
 <?php
 
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
 it('verifies if the public.animal.index route dislays correctly the animal.index view', function (){
 
     $response = \Pest\Laravel\get(route('public.animals.index'));
