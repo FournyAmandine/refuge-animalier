f2c7eaedfaaf63f50d19b54a6aa779af1c8e31d8
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-25 12:38:20 +0100

    Change the class of the button component

--- resources/views/components/public/form/buttons/button.blade.php
+++ resources/views/components/public/form/buttons/button.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button class="{!! $class ?? 'text-orange-50 bg-orange-600 border-2 rounded-lg mt-11 px-5 py-2 hover:border-orange-600 hover:orange-600 transition' !!}" type="submit">
+    <button class="{!! $class ?? 'text-orange-50 bg-orange-600 border-2 rounded-lg px-5 py-2 hover:border-orange-600 hover:orange-600 transition' !!}" type="submit">
         {!! $text !!}
     </button>
 </div>
