270f4d68d8526348d6f4236620c9a0cfebacc770
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-25 12:37:22 +0100

    Modify my slider component

--- resources/views/components/public/sections/slider.blade.php
+++ resources/views/components/public/sections/slider.blade.php
@@ -1,9 +1,9 @@
 @props(['cards'])
 
 
-<section class="pb-26 slider">
-    <h2 class="mb-2.5 text-orange-600 font-[Baloo] font-extrabold px-[1.25rem]">Nos derniers arrivants</h2>
-    <div class="grid grid-cols-[repeat(4,310px)] gap-5 overflow-x-scroll py-5 px-[1.25rem]">
+<section class="slider">
+    <h2 class="text-orange-600 font-[Baloo] font-extrabold px-[1.25rem]">Nos derniers arrivants</h2>
+    <div class="grid grid-cols-[repeat(4,310px)] gap-5 overflow-x-scroll py-4 px-[1.25rem]">
         <x-public.cards.cards_slider :cards="$cards"/>
     </div>
 </section>
