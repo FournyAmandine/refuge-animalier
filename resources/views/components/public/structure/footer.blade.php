3a49d3e0e3a4c0364fcdeb752dc5442797645381
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-26 15:56:48 +0100

    Make the footer responsive

--- resources/views/components/public/structure/footer.blade.php
+++ resources/views/components/public/structure/footer.blade.php
@@ -7,23 +7,24 @@
     ];
 @endphp
 
-<footer class="bg-white flex flex-col border-t-2 border-orange-600">
+<footer class="bg-white flex flex-col  border-t-2 border-orange-600">
     <h2 class="sro">Footer des Pattes Heureuses</h2>
-    <figure>
+    <div class="flex flex-col md:flex-row md:flex-wrap lg:justify-between">
+    <figure class="md:w-1/2 md:pt-25 xl:w-1/5">
         <img class="m-auto" src="{!! asset('assets/img/logo-medium.svg') !!}" alt="Logo Les Pattes Heureuses avec une patte de chat dans une maison" width="305" height="115">
     </figure>
-    <div class="mt-[4.375rem] flex flex-col items-center gap-[0.625rem]">
+    <div class="mt-[4.375rem] flex flex-col items-center gap-[0.625rem] md:w-1/2 xl:w-1/5 lg:pl-20">
         <h3 class="text-orange-600 font-semibold text-xl text-center mb-[0.9375rem]">Coordonnées</h3>
         <ul>
             @foreach($details as $detail)
                 <x-public.navigation.link :li_class="$detail['li_class']" :href="$detail['href']" :label="$detail['label']" :title="$detail['title']"/>
             @endforeach
         </ul>
-
     </div>
-    <div class="my-[4.375rem] flex flex-col items-center">
+
+    <div class="my-[4.375rem] flex flex-col items-center sm:w-1/2 m-auto xl:w-2/6">
         <h3 class="text-orange-600 font-semibold text-xl mb-[0.9375rem]">Heures d'ouverture</h3>
-        <ul class="flex flex-wrap max-w-9/12 gap-x-[2.5rem] gap-y-[0.9375rem]">
+        <ul class="flex flex-wrap max-w-9/12 gap-x-[2.5rem] gap-y-[0.9375rem] lg:pl-12">
             <li class="w-33">Lundi&nbsp;:&nbsp;10h-15h</li>
             <li>Mardi&nbsp;:&nbsp;9h-17h</li>
             <li class="w-34">Mercredi &nbsp;:&nbsp;9h-17h</li>
@@ -33,11 +34,14 @@
             <li class="w-40">Dimanche&nbsp;:&nbsp;11h-17h</li>
         </ul>
     </div>
-    <div class="mb-[1.875rem] px-[3.4375rem]">
+
+    <div class="mb-[1.875rem] md:my-[4.375rem] px-[3.4375rem] sm:w-1/2 m-auto md:px-0 md:w-2/5 xl:w-1/4 lg:px-[3.4375rem]">
         <h3 class="text-orange-600 font-semibold text-xl text-center">Navigation</h3>
        <x-public.navigation.links nav_class="footer_nav" li_class="footer_link"/>
     </div>
-    <div class="flex justify-end pt-[0.625rem] pr-[0.625rem] pb-[1rem] border-t-2 border-orange-600">
+
+
+    <div class="flex justify-end pt-[0.625rem] pr-[0.625rem] pb-[1rem] border-t-2 border-orange-600 md:w-1/1">
         <a class="text-sm" href="#">© Les Pattes Heureuses - Confidentialité </a>
     </div>
 </footer>
