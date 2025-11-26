cf55766b1d4051d2038f590661043bf1283a361b
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-26 11:18:31 +0100

    Make a test for the animals' status

---
+++ tests/Feature/Views/AnimalShowTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Enum\AnimalStatus;
+use App\Models\Animal;
+
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('Verify that the user can access the animal’s details page correctly and that the information is accurate', function (){
+    $animal = Animal::factory()->create(
+        ['state' => AnimalStatus::Available]
+    )->toArray();
+
+    $second_animal =Animal::factory()->create(
+        ['state' => AnimalStatus::Available]
+    )->toArray();
+
+    $response = $this->get(route('public.animals.show', $animal['id']));
+
+    $response->assertStatus(200);
+
+    $response->assertSee($animal['name']);
+
+    $response->assertSee($second_animal['name']);
+
+});
