bf7b485e1809c0b41db377c45ddc6234f22d9a3a
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-26 10:55:31 +0100

    Add span with required symbol

--- resources/views/components/public/form/fields/input.blade.php
+++ resources/views/components/public/form/fields/input.blade.php
@@ -1,7 +1,7 @@
 @props(['field_name', 'type', 'placeholder', 'required', 'label'])
 
 <div class="flex flex-col flex-1 pb-5">
-    <label class="font-semibold pb-1" for="{!! $field_name !!}">{!! $label !!}</label>
+    <label class="font-semibold pb-1" for="{!! $field_name !!}">{!! $label !!} <span class="text-red-600 required">*</span></label>
     <input class="border-2 border-orange-600/80 bg-orange-600/10 rounded-lg p-2"
            type="{!! $type ?? 'text' !!}"
            {{$value ?? old($field_name)}}
--- resources/views/components/public/form/fields/textarea.blade.php
+++ resources/views/components/public/form/fields/textarea.blade.php
@@ -1,6 +1,6 @@
 @props(['field_name', 'placeholder', 'label'])
 
 <div class="flex flex-col pb-5">
-    <label class="font-semibold pt-1" for="{!! $field_name !!}">{!! $label !!}</label>
+    <label class="font-semibold pt-1" for="{!! $field_name !!}">{!! $label !!} <span class="text-red-600 required">*</span></label>
     <textarea name="{!! $field_name !!}" id="{!! $field_name !!}" cols="25" rows="5" class="border-2 border-orange-600/80 bg-orange-600/10 rounded-lg p-2" placeholder="{!! $placeholder !!}"></textarea>
 </div>
