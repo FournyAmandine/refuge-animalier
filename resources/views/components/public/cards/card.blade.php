884d197e9bf97b441043311d6733bffce60b920a
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-26 10:33:36 +0100

    Make the animal card clickable

--- resources/views/components/public/cards/card.blade.php
+++ resources/views/components/public/cards/card.blade.php
@@ -10,11 +10,14 @@
 
 
 <article class="bg-orange-100 border-2 border-orange-600 max-w-77.5 rounded-xl relative shadow-[var(--shadow-xl)]">
-    <div class="relative">
-        <x-public.cards.card_img :src="$src" :alt="$alt" :name="$name" />
-    </div>
-    <div class="pt-4 pb-3 px-5">
-        <x-public.cards.card_definition :definitions="$definitions" :dd="$dd"/>
-        <x-public.cards.card_button label="Découvrir ce compagnon" :href="$href" :title="$title"/>
+    <a class="absolute z-2 inset-0" title="{!! $title !!}" href="{!! $href !!}"><span>Voir la fiche de l'animal</span></a>
+    <div>
+        <div class="relative">
+            <x-public.cards.card_img :src="$src" :alt="$alt" :name="$name" />
+        </div>
+        <div class="pt-4 pb-3 px-5">
+            <x-public.cards.card_definition :definitions="$definitions" :dd="$dd"/>
+            <x-public.cards.card_button label="Découvrir ce compagnon" :href="$href" :title="$title"/>
+        </div>
     </div>
 </article>
