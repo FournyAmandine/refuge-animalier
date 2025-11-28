aa9d62a5c203b59004574143a209386964af0a1f
FournyAmandine <amandine.fourny@student.hepl.be>
2025-11-28 09:36:03 +0100

    Add the title in the section

--- resources/views/components/public/sections/card-list.blade.php
+++ resources/views/components/public/sections/card-list.blade.php
@@ -3,12 +3,15 @@
     'animals'
 ])
 
-<section {{ $attributes->class(['pt-10 flex flex-col gap-10 items-center']) }}>
-    @foreach($animals as $animal)
-        <x-public.cards.card src="{!! $animal->img_path !!}"
-                             alt="Photo de {!! $animal->name !!}"
-                             title="{!! $animal->name !!}"
-                             href="{!! route('public.animals.show', $animal->id) !!}"
-                             name="{!! $animal->name !!}" :dd="[$animal->sexe,$animal->age . 'ans',$animal->race]"/>
-    @endforeach
+<section>
+    <h2 class="text-orange-600 text-xl">Nos derniers arrivants</h2>
+    <div {{ $attributes->class(['pt-10 flex flex-col gap-10 items-center']) }}>
+        @foreach($animals as $animal)
+            <x-public.cards.card src="{!! $animal->img_path !!}"
+                                 alt="Photo de {!! $animal->name !!}"
+                                 title="{!! $animal->name !!}"
+                                 href="{!! route('public.animals.show', $animal->id) !!}"
+                                 name="{!! $animal->name !!}" :dd="[$animal->sexe,$animal->age . 'ans',$animal->race]"/>
+        @endforeach
+    </div>
 </section>
