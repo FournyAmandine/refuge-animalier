a014024040929f2a99552f8ae109e5f0e9033e18
FournyAmandine <amandine.fourny@student.hepl.be>
2025-12-12 11:05:56 +0100

    Run the test and make the route

--- resources/views/pages/animals/⚡index/index.test.php
+++ resources/views/pages/animals/⚡index/index.test.php
@@ -2,7 +2,8 @@
 
 use Livewire\Livewire;
 
-it('renders successfully', function () {
+it('verifies if the /admin/animals/index dislays correctly the admin.animals.index view', function () {
     Livewire::test('pages::animals.index')
-        ->assertStatus(200);
+        ->assertStatus(200)
+        ->assertSeeLivewire('pages::animals.index');
 });
