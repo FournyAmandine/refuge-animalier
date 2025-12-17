037be1af255ff7c0a98812ad1b5c6fa5eae2f86b
FournyAmandine <amandine.fourny@student.hepl.be>
2025-12-17 14:27:06 +0100

    Add the messages index page

--- resources/views/pages/messages/⚡index/index.blade.php
+++ resources/views/pages/messages/⚡index/index.blade.php
@@ -1,4 +1,15 @@
-
-<div>
-    {{-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius --}}
-</div>
+<main class="lg:flex-1 bg-orange-50/30">
+    <x-admin.sections.intro ariane="Messages" title="Vos messages de contact"/>
+    <section>
+        <h3 class="title_section pb-5">Non-lus</h3>
+        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600"/>
+        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600"/>
+        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600"/>
+    </section>
+    <section class="pt-15">
+        <h3 class="title_section">Lus</h3>
+        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600"/>
+        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600"/>
+        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600"/>
+    </section>
+</main>
