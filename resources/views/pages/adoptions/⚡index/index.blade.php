2c961edab1923d580d64f80312ffe22a2c410fa0
FournyAmandine <amandine.fourny@student.hepl.be>
2025-12-17 15:05:09 +0100

    Update the component

--- resources/views/pages/adoptions/⚡index/index.blade.php
+++ resources/views/pages/adoptions/⚡index/index.blade.php
@@ -2,14 +2,18 @@
     <x-admin.sections.intro ariane="Adoptions" title="Vos demandes d'adoption"/>
     <section>
         <h3 class="title_section pb-5 lg:text-2xl">Non-validées</h3>
-        <x-admin.adoptions.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
-        <x-admin.adoptions.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
-        <x-admin.adoptions.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
     </section>
     <section class="pt-15">
         <h3 class="title_section pb-5 lg:text-2xl">Validées</h3>
-        <x-admin.adoptions.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-        <x-admin.adoptions.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-        <x-admin.adoptions.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
+        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
     </section>
 </main>
