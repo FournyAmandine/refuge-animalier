a76ed0451182489a07d6157307a8963a4c7f4467
FournyAmandine <amandine.fourny@student.hepl.be>
2025-12-17 16:32:25 +0100

    Refactor the code in the components

--- resources/views/pages/adoptions/⚡index/index.blade.php
+++ resources/views/pages/adoptions/⚡index/index.blade.php
@@ -1,19 +1,5 @@
 <main class="lg:flex-1 bg-orange-50/30">
     <x-admin.sections.intro ariane="Adoptions" title="Vos demandes d'adoption"/>
-    <section>
-        <h3 class="title_section pb-5 lg:text-2xl">Non-validées</h3>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Valider"/>
-    </section>
-    <section class="pt-15">
-        <h3 class="title_section pb-5 lg:text-2xl">Validées</h3>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-        <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-    </section>
+    <x-admin.sections.adoptions.non_validated_adoptions/>
+    <x-admin.sections.adoptions.validated_adoptions/>
 </main>
--- resources/views/pages/messages/⚡index/index.blade.php
+++ resources/views/pages/messages/⚡index/index.blade.php
@@ -1,15 +1,5 @@
 <main class="lg:flex-1 bg-orange-50/30">
     <x-admin.sections.intro ariane="Messages" title="Vos messages de contact"/>
-    <section>
-        <h3 class="title_section pb-5">Non-lus</h3>
-        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Répondre"/>
-        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Répondre"/>
-        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-red-600" label="Répondre"/>
-    </section>
-    <section class="pt-15">
-        <h3 class="title_section pb-5">Lus</h3>
-        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600" label="Répondre"/>
-        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600" label="Répondre"/>
-        <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600" label="Répondre"/>
-    </section>
+    <x-admin.sections.messages.non_read_messages/>
+    <x-admin.sections.messages.read_messages/>
 </main>
