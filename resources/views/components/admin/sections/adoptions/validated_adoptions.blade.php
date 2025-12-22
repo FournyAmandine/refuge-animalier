10f4e7e728bdb733ad23eb937595ad02dd8c187e
FournyAmandine <amandine.fourny@student.hepl.be>
2025-12-22 10:42:01 +0100

    Delete unused files

--- resources/views/components/admin/sections/adoptions/validated_adoptions.blade.php
+++
@@ -1,8 +0,0 @@
-<section class="pt-15">
-    <h3 class="title_section pb-5 lg:text-2xl">Validées</h3>
-    <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-    <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-    <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-    <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-    <x-admin.adoptions.article_card animal="Moka" name="Sarah Deseurs" day="3" class="border-green-600" label="Annuler la validation"/>
-</section>
--- resources/views/components/admin/sections/messages/read_messages.blade.php
+++
@@ -1,6 +0,0 @@
-<section class="pt-15">
-    <h3 class="title_section pb-5 lg:text-2xl">Lus</h3>
-    <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600" label="Répondre"/>
-    <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600" label="Répondre"/>
-    <x-admin.messages.article_card message="Bonjour, je voudrais voir Moka..." name="Sarah Deseurs" day="3" email="sarahdeseurs@gmail.com" class="border-green-600" label="Répondre"/>
-</section>
