21e6a0081d63cf611e631f37203484b929d09811
FournyAmandine <amandine.fourny@student.hepl.be>
2025-12-22 10:44:44 +0100

    Make the adoptions index page and the modal

--- resources/views/components/admin/adoptions/article_card.blade.php
+++ resources/views/components/admin/adoptions/article_card.blade.php
@@ -1,12 +1,12 @@
-@props(['name', 'animal', 'day', 'label'])
+@props(['name', 'animal', 'day', 'label', 'id'])
 
 
 <article {!! $attributes->merge(['class'=>'relative hover:scale-105 hover:duration-200 hover:transition-all flex flex-wrap justify-between items-center border-2  rounded-lg p-2.5 [box-shadow:var(--shadow-xl)] mb-5 min-[1920px]:max-w-[64rem]']) !!}>
-    <a class="absolute inset-0" href="#" title="Voir la demande d'adoption"></a>
+    <a class="absolute inset-0" wire:click="toggleModal('show', {{$id}})" href="#" title="Voir la demande d'adoption"></a>
     <div class="pb-2.5">
         <h4 class="lg:text-xl">{!! $name !!}</h4>
         <p class="text-sm">Souhaite adopter {!! $animal !!}</p>
     </div>
     <p class="text-sm">Il y a {!! $day !!} jours</p>
-    <x-admin.button href="#" title="Valider la demande d'adoption pour {!! $name !!}" label="{!! $label !!}" class="pr-3 pl-15 validate"/>
+    <x-admin.modal.button wire:click="toggleValidateAdoption({{$id}})" title="Valider la demande d'adoption pour {!! $name !!}" label="{!! $label !!}" class="pr-3 pl-15 validate"/>
 </article>
--- resources/views/pages/adoptions/⚡index/index.php
+++ resources/views/pages/adoptions/⚡index/index.php
@@ -1,8 +1,43 @@
 <?php
 
+use App\Models\Adoption;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public bool $isOpenShowModal = false;
+
+    public string|Adoption $openAdoption = '';
+
+    public function toggleValidateAdoption(Adoption $adoptions){
+        $adoptions->update([
+            'validate'=> !$adoptions->validate,
+        ]);
+    }
+    public function render()
+    {
+        return view('pages.adoptions.⚡index.index', [
+            'adoptions_non_validate' => Adoption::orderBy('created_at', 'desc')->with('animal')->where('adoptions.validate', 0)->get(),
+            'adoptions_validate' => Adoption::orderBy('created_at', 'desc')->with('animal')->where('adoptions.validate', 1)->get(),
+        ]);
+    }
+
+    #[On ('toggleModal')]
+    public function toggleModal(string $modal, $id = ''): void
+    {
+        if ($modal === 'show') {
+            $this->isOpenShowModal = !$this->isOpenShowModal;
+        }
+
+        $this->isOpenShowModal? $this->dispatch('open-modal') : $this->dispatch('close-modal');
+        $this->openAdoption = $id !== '' ? Adoption::find($id) : '';
+    }
+
+    public function markValidate():void
+    {
+        $this->toggleValidateAdoption($this->openAdoption);
+        $this->dispatch('close-modal');
+        $this->toggleModal('show');
+    }
 };
