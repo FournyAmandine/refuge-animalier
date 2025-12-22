3c56a348ccc7cc2043a8ac1c1df272fa8304bcbb
FournyAmandine <amandine.fourny@student.hepl.be>
2025-12-22 10:44:11 +0100

    Add a relation

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Animal extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'sexe', 'race', 'type', 'coat', 'vaccines', 'birth_date', 'state', 'img_path', 'description'];
+
+    public function animals():HasMany
+    {
+        return $this->hasMany(Adoption::class);
+    }
 }
