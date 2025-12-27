ac46bd5ccf550ed1821398fcff3a220c15bfb6f2
FournyAmandine <amandine.fourny@student.hepl.be>
2025-12-27 14:58:22 +0100

    Add middleware

---
+++ app/Http/Middleware/IsAdministrator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->user()->role !== 'Administrateur') {
+            return response()->view('errors.not_authorized', [], 403);
+        }
+        return $next($request);
+    }
+}
