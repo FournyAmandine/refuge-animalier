c9db44ac6b92157c77a7e82b56dd460b849f9d2b
FournyAmandine <amandine.fourny@student.hepl.be>
2026-01-03 03:19:32 +0100

    Add sort in admin volunteers index

--- resources/views/components/admin/table/volunteers/thead.blade.php
+++ resources/views/components/admin/table/volunteers/thead.blade.php
@@ -1,11 +1,11 @@
 @php
     $titles_col = [
-        ['label' => __('volunteer_index.photo'), 'tri' => 'false'],
-        ['label' => __('volunteer_index.last_name'), 'tri' => 'true'],
-        ['label' => __('volunteer_index.first_name'), 'tri' => 'true'],
-        ['label' => __('volunteer_index.email'), 'tri' => 'true'],
-        ['label' => __('volunteer_index.joined_at'), 'tri' => 'true'],
-        ['label' => __('volunteer_index.actions'), 'tri' => 'false'],
+        ['label' => __('volunteer_index.photo'), 'tri' => false],
+        ['label' => __('volunteer_index.last_name'), 'tri' => 'last_name'],
+        ['label' => __('volunteer_index.first_name'), 'tri' => 'first_name'],
+        ['label' => __('volunteer_index.email'), 'tri' => 'email'],
+        ['label' => __('volunteer_index.joined_at'), 'tri' => 'created_at'],
+        ['label' => __('volunteer_index.actions'), 'tri' => false],
     ];
 @endphp
 
@@ -15,12 +15,12 @@
         <th scope="col" class="py-4 px-6 text-center font-semibold">
             <div class="flex items-center justify-center">
                 {!! $title_col['label'] !!}
-                @if($title_col['tri'] == 'true')
-                    <a href="#">
+                @if($title_col['tri'])
+                    <button wire:click="sortBy('{{ $title_col['tri'] }}')">
                         <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="#401B03" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
                         </svg>
-                    </a>
+                    </button>
                 @endif
             </div>
         </th>
